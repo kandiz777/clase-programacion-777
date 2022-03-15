3901d3d7fba0d02e6e0ad06db1ff9994d919fa5e
kandiz777 <kandes310507@gmail.com>
2022-03-15 11:24:52 -0500

    save

--- index.js
+++ index.js
@@ -38,8 +38,8 @@ console.log(nombre+" "+apellido);
 var edad=13; 
 var edadEstudiante=edad;
 //desde el teclado 
-var gradoEstudiante=prompt("ingrese su grado: ");
-console.log("el grado es: "+gradoEstudiante);
+//var gradoEstudiante=prompt("ingrese su grado: ");
+//console.log("el grado es: "+gradoEstudiante);
 /*constantes:
 son valores que una vezz inicializados no pueden cambiar lo que almacenan. Se crean con la palabra reservada const, el nombre de las constantes se escribe todo en mayuscula para diferenciarlo de las variables
 */
@@ -52,3 +52,135 @@ console.log("mensaje");
 console.log(nombre);
 //concantenado
 console.log("la edad es "+ edad +" años");
+
+//operadores
+
+//suma
+console.log("------------------------------");
+console.log("Suma");
+
+var suma;
+suma = 5+3;
+console.log(suma);
+let a =10;
+suma =8+a;
+console.log (suma);
+let b =20;
+suma =a+b;
+console.log(suma);
+console.log("------------------------------");
+console.log("Resta");
+
+//resta
+var resultado;
+resultado = a-b;
+console.log(resultado);
+resultado =b-a;
+console.log(resultado);
+console.log("------------------------------");
+console.log("Multiplicacion");
+
+//producto
+producto = a*b;
+console.log(producto);
+console.log("------------------------------");
+console.log("Division");
+
+//Division
+division = b/a;
+console.log(division);
+console.log("------------------------------");
+
+// orden de operaciones
+var res;
+res =a*2;
+console.log(res);
+res =a*2-1;
+console.log(res);
+res =a*(2-1);
+console.log(res);
+console.log("------------------------------");
+console.log("residuo");
+
+//residuo
+var residuo;
+residuo =12%2; //0
+console.log(residuo);
+console.log("------------------------------");
+console.log("incremento");
+
+//contador
+//incremento(+)
+var incremento =1;
+console.log(incremento);
+incremento =incremento+1;
+console.log(incremento);
+incremento +=1;
+console.log(incremento);
+incremento++;
+console.log(incremento);
+console.log("------------------------------");
+console.log("decremento");
+
+//decremento(-)
+var decremento =1;
+console.log(decremento);
+decremento -=1;
+console.log(decremento)
+decremento -=1;
+console.log(decremento)
+decremento -=1;
+console.log(decremento)
+decremento -=1;
+console.log(decremento)
+console.log("------------------------------");
+console.log("suma aumento");
+
+//son operaciones que se le realizan a una variable y el resultado se guarda en si misma
+
+var sumaaumento=0;
+sumaaumento += 4;
+sumaaumento += 46;
+console.log(sumaaumento);
+console.log("------------------------------");
+console.log("producto aumento");
+
+var productoaumento=0;
+productoaumento *= 4;
+productoaumento *= 46;
+console.log(productoaumento);
+console.log("------------------------------");
+console.log("cocienteaumento");
+
+var cocienteaumento=0;
+cocienteaumento /= 4;
+cocienteaumento /= 46;
+console.log(cocienteaumento);
+console.log("------------------------------");
+console.log("potencia");
+
+//potencia
+var exp;
+exp =3**2;
+console.log(exp);
+exp =3**3
+console.log(exp)
+// la variable debe tener un valop antes de realizar la operacion
+
+var nombre = ("Oscar")
+var apellido = ("Salgado")
+var nombreyapellido = (nombre + " " + apellido)
+console.log(nombreyapellido)
+console.log("Cambiaras tu nombre!")
+var nombreyapellido = ("Carlos salgado")
+console.log(nombreyapellido)
+
+//Los numeros en las variables no van con comillas si las usas seria solo para strings  = "palabras"
+
+var oscar1 = 15;
+var akio1 = 15;
+var diazcuenta = akio1 + oscar1;
+console.log(diazcuenta);
+
+console.log(nombreyapellido.length)
+
---
+++ taller2.js
@@ -0,0 +1,67 @@
+
+console.log("------------------------------");
+console.log("area cuadrado");
+var lado = prompt ("cual es el valor que desea darle al lado");
+var areacuadrado = lado **2;
+console.log("el resultado del area es el siguiente")
+console.log(areacuadrado);
+
+console.log("------------------------------");
+console.log("volumen cuadrado");
+var  lado = prompt (" cual es la medida del lado del cubo");
+var volumen = lado **3 ;
+console.log("el resultado del volumen es el siguiente")
+console.log(volumen)
+console.log("resultado del punto #1");
+
+console.log("------------------------------");
+console.log("area rectangulo");
+var base= prompt (" cual es la base del rectangulo")
+var altura = prompt ("cual es la altura")
+var arearectangulo = base * altura
+console.log("el area del rectangulo es")
+console.log( arearectangulo)
+console.log("------------------------------");
+console.log("perimetro rectangulo");
+var perimetro= 2*altura + 2*base
+console.log(perimetro )
+console.log("resultado del punto #2");
+
+console.log("------------------------------");
+console.log("area circulo");
+var radio = prompt ("escriba el valor del radio del circulo")
+var areacirculo = Math.PI*radio**2
+console.log(areacirculo)
+
+console.log("------------------------------");
+console.log("volumen esfera");
+var volumen = 4/3 * Math.PI * radio **3
+console.log(" el volumen es ")
+console.log( volumen)
+console.log ("resutado del punto #3")
+
+console.log("------------------------------")
+console.log( "trapezoide")
+var base1 = prompt ("ingrese el valor de la primera base")
+var base2 = prompt (" ingrese el valor de la segunda base")
+var h = prompt ( "ingrese el valor de h")
+var area = 1/2 * (base1 + base2) * h
+console.log (area)
+console.log ("resultado del punto #4")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
