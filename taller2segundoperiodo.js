18cd28105a2ecd8f46e9f4f6c82b1e4560d8c654
kandiz777 <kandes310507@gmail.com>
2022-05-04 10:22:11 -0500

    taller2periodo

--- taller2segundoperiodo.js
+++ taller2segundoperiodo.js
@@ -14,7 +14,7 @@ var edad = 18;
 if (edad>=18) {
     console.log("Bienvenido, puede ingresar")
     }else {
-        (edad<18)
+       
         console.log("no puede ingresar")
     }
 
@@ -39,9 +39,52 @@ var numero2=8
         console.log("incrementar en una unidad el valor del numero1 no lo hace mayor o igual a numero2")
       }
 
+var numerito = 90;
+if (numerito < 50) {
+    console.log("numerito si es estrictamente menor que 50");
+} else if (numerito > 50 && numerito < 80) {
+    console.log("numerito si esta entre 50 y 80") 
+} else if (numerito > 80 &&  !(numerito >= 100)) {
+    console.log("numerito es mayor que 80 pero no mayor ni igual a 100")
+} else if (numerito == 100) {
+    console.log("numerito es igual a 100")
+} else {
+    console.log("numerito es estrictamente mayor a 100")
+}
+
+// Punto 6 
+    numerito = 90;
+//Punto 7
+    numerito = 60;
+//Punto 8 
+    numerito = 200;
+
+//Punto 9
+
+var dia = "lunes";
+
+if (dia == "jueves") {
+    console.log("Pueden ir a polideportivo")
+} else {
+    console.log("No pueden ir al polideportivo")
+}
+
+//Punto 10
+
+var x1 = 10
+var x2 = 15
+var resultado = x1/x2 
+
+if (x1 == 0 || x2 == 0) {
+    console.log("No se puede hacer una division con cero")
+} else {
+    console.log("El resultado es " + resultado)
+}
+
+//Punto 11
 
 
-      var numerito = 100
 
-      if() {
-          console.log("numerito si es estrictamente menor ")
+
+
+
